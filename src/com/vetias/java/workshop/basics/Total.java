1fb9b5a585373a7b0659f3d6b3261076c2753408
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-02 15:30:34 +0530

    update the code

--- src/com/vetias/java/workshop/basics/Total.java
+++ src/com/vetias/java/workshop/basics/Total.java
@@ -1,5 +1,7 @@
 public class Total {
     public static void main(String[] args) {
-        int a=70,b=60,c=50,d=40
+        int a=70,b=60,c=50,d=40,e=30;
+        int Total=a+b+c+d+e;
+        System.out.println("Total:"+Total);
     }
 }
