5377732ca3a5160555cdd7300e252bd2ed8ba28d
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-02 15:19:54 +0530

    update to git

---
+++ src/com/vetias/java/workshop/basics/Marks.java
@@ -0,0 +1,8 @@
+public class Marks {
+    public static void main(String[] args) {
+        int[]Marks={50,60,70,80}
+        for(int Mark:Marks){
+            System.out.println(Marks);
+        }
+    }   
+}
---
+++ src/com/vetias/java/workshop/basics/Total.java
@@ -0,0 +1,5 @@
+public class Total {
+    public static void main(String[] args) {
+        int a=70,b=60,c=50,d=40
+    }
+}
