3fb814c16e4bb753f73f7bee7dd8b3f47dc9294c
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-07 14:27:57 +0530

    update to git

---
+++ src/com/vetias/java/workshop/basics/stringbuilderexample.java
@@ -0,0 +1,17 @@
+
+
+public class stringbuilderexample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" java");
+        sb.append(" is");
+        sb.append(" the");
+        sb.append(" most");
+        sb.append(" used");
+        sb.append(" language");
+        sb.append(" for");
+        sb.append(" enterprise");
+        sb.append(" applications.6t53");
+        System.out.println(sb.toString());                 
+    }
+}
