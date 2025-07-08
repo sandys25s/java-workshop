ad8f7fa90a36a60b87c2ffe3aa5a06ffb3b94aa7
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-08 10:20:23 +0530

    update to git

--- src/com/vetias/java/workshop/basics/Agecalculator.java
+++ src/com/vetias/java/workshop/basics/Agecalculator.java
@@ -1,6 +1,7 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 public class Agecalculator {
-    public static void main(String[] args) {
+    public static <localDate> void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int birthyear;
         System.out.print("Enter your birth year: ");
@@ -8,6 +9,15 @@ public class Agecalculator {
         int currentyear = 2025;
         int Age = currentyear - birthyear;
         System.out.println("Your age is: " + Age);
+
+
+        System.out.println("enter your month of birth (1-12): ");
+        String datebrith = scanner.next();
+        LocalDate today = LocalDate.parse(datebrith);
+        int yearbrith = today.getYear();
+        int currentYearNow = LocalDate.now().getYear();
+        int age = currentYearNow - yearbrith;
+        System.out.println("Your age is: " + age + " years");
         scanner.close();
     }
 }
