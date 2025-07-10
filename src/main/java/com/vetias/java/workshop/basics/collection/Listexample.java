68d4a8e5b27b11452bcaeed317dc44b976551c2f
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-10 15:07:11 +0530

    update to git

--- src/main/java/com/vetias/java/workshop/basics/collection/Listexample.java
+++ src/main/java/com/vetias/java/workshop/basics/collection/Listexample.java
@@ -1,10 +1,12 @@
 package com.vetias.java.workshop.basics.collection;
-    import java.util.ArrayList;
-    import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Listexample {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<p>();
+        List<String> list = new ArrayList<String>(); // Corrected type
+
         list.add("Hello");
         list.add("World");
         list.add("Java");
@@ -12,9 +14,8 @@ public class Listexample {
         list.add("Collections");
 
         System.out.println("names");
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
     }
-
 }
---
+++ src/main/java/com/vetias/java/workshop/basics/collection/Studentset.java
@@ -0,0 +1,22 @@
+package com.vetias.java.workshop.basics.collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Studentset {
+    public static void main(String[] args) {
+        Set<Integer> rollnos = new HashSet<Integer>();
+
+        rollnos.add(1); // Corrected: use integers, not strings
+        rollnos.add(2);
+        rollnos.add(3);
+        rollnos.add(4);
+        rollnos.add(5);
+
+        System.out.println("Set content: " + rollnos);
+        System.out.println("Iterating through set:");
+        for (Integer rollno : rollnos) {
+            System.out.println(rollno);
+        }
+    }
+}
