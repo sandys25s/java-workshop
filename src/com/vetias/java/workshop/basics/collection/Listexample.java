a7d37265c016978e2038fe6af33851a8b3f40648
sandys25s <santhoshvinayagams24aid@vetias.ac.in>
2025-07-10 14:41:51 +0530

    update to git

---
+++ src/com/vetias/java/workshop/basics/collection/Listexample.java
@@ -0,0 +1,20 @@
+package com.vetias.java.workshop.basics.collection;
+    import java.util.ArrayList;
+    import java.util.List;
+
+public class Listexample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<p>();
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+        list.add("Programming");
+        list.add("Collections");
+
+        System.out.println("names");
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+}
